d960492f1f118b88ab7de41c33bc27c4d7f653bd
Lucas Soares <lucas.soares@viprede.com>
2023-09-03 17:59:43 -0300

    verify item length getuser

--- src/controller/User.js
+++ src/controller/User.js
@@ -18,44 +18,48 @@ const User = {
         return ("Error Request User: ", error);
       } else {
 
-        if (results[0].direcAcesso == 1) {
+        if (results.length > 0) {
+          if (results[0].direcAcesso == 1) {
 
-          const queryProvider = "select acesso.codAcesso, acesso.direcAcesso, fornecedor.nomeForn, fornecedor.cnpjForn, acesso.codUsuario, fornecedor.codForn, consultor.nomeConsult, consultor.cpfConsult, FORMAT(IFNULL(sum(mercadoria.precoMercadoria*pedido.quantMercPedido), 0), 2, 'de_DE') as 'valorPedido' from acesso join consultor on acesso.codUsuario = consultor.codConsult join relacionaFornecedor on consultor.codConsult = relacionaFornecedor.codConsultor	join fornecedor on relacionaFornecedor.codFornecedor = fornecedor.codForn left join pedido on pedido.codFornPedido = fornecedor.codForn left join mercadoria on mercadoria.codMercadoria = pedido.codMercPedido where acesso.codAcesso = " + codacesso;
+            const queryProvider = "select acesso.codAcesso, acesso.direcAcesso, fornecedor.nomeForn, fornecedor.cnpjForn, acesso.codUsuario, fornecedor.codForn, consultor.nomeConsult, consultor.cpfConsult, FORMAT(IFNULL(sum(mercadoria.precoMercadoria*pedido.quantMercPedido), 0), 2, 'de_DE') as 'valorPedido' from acesso join consultor on acesso.codUsuario = consultor.codConsult join relacionaFornecedor on consultor.codConsult = relacionaFornecedor.codConsultor	join fornecedor on relacionaFornecedor.codFornecedor = fornecedor.codForn left join pedido on pedido.codFornPedido = fornecedor.codForn left join mercadoria on mercadoria.codMercadoria = pedido.codMercPedido where acesso.codAcesso = " + codacesso;
 
-          connection.query(queryProvider, (error, results) => {
-            if (error) {
-              return ("Error Insert User Client: ", error);
-            } else {
-              return res.json(results);
-            }
-          });
-          // connection.end();
+            connection.query(queryProvider, (error, results) => {
+              if (error) {
+                return ("Error Insert User Client: ", error);
+              } else {
+                return res.json(results);
+              }
+            });
+            // connection.end();
 
-        } else if (results[0].direcAcesso == 2) {
+          } else if (results[0].direcAcesso == 2) {
 
-          const queryClient = "SELECT acesso.codAcesso, acesso.direcAcesso, associado.razaoAssociado AS nomeForn, associado.cnpjAssociado AS cnpjForn, acesso.codUsuario, associado.codAssociado AS codForn, consultor.nomeConsult, consultor.cpfConsult, FORMAT(IFNULL(sum(mercadoria.precoMercadoria*pedido.quantMercPedido), 0), 2, 'de_DE') as 'valorPedido' FROM acesso join consultor on acesso.codUsuario = consultor.codConsult join relaciona on relaciona.codAssocRelaciona = consultor.codConsult join associado on associado.codAssociado = relaciona.codConsultRelaciona left join pedido on pedido.codAssocPedido = associado.codAssociado left join mercadoria on mercadoria.codMercadoria = pedido.codMercPedido WHERE acesso.codAcesso = " + codacesso;
+            const queryClient = "SELECT acesso.codAcesso, acesso.direcAcesso, associado.razaoAssociado AS nomeForn, associado.cnpjAssociado AS cnpjForn, acesso.codUsuario, associado.codAssociado AS codForn, consultor.nomeConsult, consultor.cpfConsult, FORMAT(IFNULL(sum(mercadoria.precoMercadoria*pedido.quantMercPedido), 0), 2, 'de_DE') as 'valorPedido' FROM acesso join consultor on acesso.codUsuario = consultor.codConsult join relaciona on relaciona.codAssocRelaciona = consultor.codConsult join associado on associado.codAssociado = relaciona.codConsultRelaciona left join pedido on pedido.codAssocPedido = associado.codAssociado left join mercadoria on mercadoria.codMercadoria = pedido.codMercPedido WHERE acesso.codAcesso = " + codacesso;
 
-          connection.query(queryClient, (error, results) => {
-            if (error) {
-              return ("Error Update User Client: ", error);
-            } else {
-              return res.json(results);
-            }
-          });
-          // connection.end();
+            connection.query(queryClient, (error, results) => {
+              if (error) {
+                return ("Error Update User Client: ", error);
+              } else {
+                return res.json(results);
+              }
+            });
+            // connection.end();
 
-        } else if (results[0].direcAcesso == 3) {
+          } else if (results[0].direcAcesso == 3) {
 
-          const queryOrganization = "SELECT acesso.codAcesso, acesso.direcAcesso, organizador.nomeOrg AS nomeForn, organizador.cnpjOrg AS cnpjForn, acesso.codUsuario,  organizador.codOrg AS codForn, consultor.nomeConsult, consultor.cpfConsult, FORMAT(IFNULL(sum(mercadoria.precoMercadoria*pedido.quantMercPedido), 0), 2, 'de_DE') as 'valorPedido' FROM acesso join consultor on acesso.codUsuario = consultor.codConsult join organizador on organizador.codOrg = consultor.codFornConsult left join pedido on pedido.codOrganizador = organizador.codOrg left join mercadoria on mercadoria.codMercadoria = pedido.codMercPedido where codOrganizador = " + results[0].codOrganization + " and acesso.codAcesso = " + codacesso;
+            const queryOrganization = "SELECT acesso.codAcesso, acesso.direcAcesso, organizador.nomeOrg AS nomeForn, organizador.cnpjOrg AS cnpjForn, acesso.codUsuario,  organizador.codOrg AS codForn, consultor.nomeConsult, consultor.cpfConsult, FORMAT(IFNULL(sum(mercadoria.precoMercadoria*pedido.quantMercPedido), 0), 2, 'de_DE') as 'valorPedido' FROM acesso join consultor on acesso.codUsuario = consultor.codConsult join organizador on organizador.codOrg = consultor.codFornConsult left join pedido on pedido.codOrganizador = organizador.codOrg left join mercadoria on mercadoria.codMercadoria = pedido.codMercPedido where codOrganizador = " + results[0].codOrganization + " and acesso.codAcesso = " + codacesso;
 
-          connection.query(queryOrganization, (error, results) => {
-            if (error) {
-              return ("Error Update User Client: ", error);
-            } else {
-              return res.json(results);
-            }
-          });
-          // connection.end();
+            connection.query(queryOrganization, (error, results) => {
+              if (error) {
+                return ("Error Update User Client: ", error);
+              } else {
+                return res.json(results);
+              }
+            });
+            // connection.end();
+          }
+        } else {
+          res.json({ results: 0 })
         }
       };
       return 0;
