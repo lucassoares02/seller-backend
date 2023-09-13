8aa7629a02a61a9d816e846417cb61eea9c0104b
Lucas Soares <lucas.soares02@hotmail.com>
2023-09-13 10:01:28 -0300

    added route users organizador

--- src/routes.js
+++ src/routes.js
@@ -25,7 +25,7 @@ router.post("/getuser", User.getUser); // localhost:3001/getuser
 
 router.post("/getuserweb", User.getUserWeb); // localhost:3001/getuser
 
-router.post("/getallusers", User.getAllUsersAccess); // localhost:3001/getuser
+router.get("/getallusers", User.getAllUsersAccess); // localhost:3001/getuser
 
 
 // Methods Clients
