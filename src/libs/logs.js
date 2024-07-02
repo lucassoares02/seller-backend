e58901a8ac5e0d673aeeef000680fa3e7613dac0
Lucas Soares <lucas.soares@viprede.com>
2024-07-02 13:53:46 -0300

    add console save logs

--- src/libs/logs.js
+++ src/libs/logs.js
@@ -1,6 +1,7 @@
 const { connection } = require("@server");
 
 async function saveLogs(user, action, platform) {
+    console.log("salvando os logs")
     const query = `INSERT INTO logs (user, action, platform) VALUES (${user}, '${action}', '${platform}')`;
     try {
         return await connection.query(query, (error, results, fields) => {
