3aae57c90d3fbc83e70d9f731808a69a8147e35e
Lucas Soares <lucas.soares@viprede.com>
2024-08-16 20:49:00 -0300

    insert request faceline

--- src/controller/Faceline.js
+++ src/controller/Faceline.js
@@ -6,41 +6,41 @@ const Faceline = {
   async insert(req, res) {
     logger.info("Post Save Faceline Client");
 
-    const { client, datetime, button1, button2, button3, button4, button5, button6, button7, button8, button_whatsApp, button_Oferta1, button_Oferta2, button_Oferta3, percentage } = data;
+    const { client, datetime, button1, button2, button3, button4, button5, button6, button7, button8, button_whatsApp, button_Oferta1, button_Oferta2, button_Oferta3, percentage } = req.body;
 
     // Verificar se o cliente já existe
     const selectQuery = `SELECT * FROM faceline WHERE client = ${client}`;
 
     connection.query(selectQuery, [client], (err, results) => {
-        if (err) throw err;
+      if (err) throw err;
 
-        if (results.length > 0) {
-            // Se o cliente existir, atualize as informações
-            const updateQuery = `
+      if (results.length > 0) {
+        // Se o cliente existir, atualize as informações
+        const updateQuery = `
                 UPDATE faceline
                 SET datetime = ?, button1 = ?, button2 = ?, button3 = ?, button4 = ?, button5 = ?, button6 = ?, button7 = ?, button8 = ?, button_whatsApp = ?, button_Oferta1 = ?, button_Oferta2 = ?, button_Oferta3 = ?, percentage = ?
                 WHERE client = ?
             `;
 
-            connection.query(updateQuery, [datetime, button1, button2, button3, button4, button5, button6, button7, button8, button_whatsApp, button_Oferta1, button_Oferta2, button_Oferta3, percentage, client], (err, results) => {
-                if (err) throw err;
-                console.log('Cliente atualizado com sucesso!');
-            });
+        connection.query(updateQuery, [datetime, button1, button2, button3, button4, button5, button6, button7, button8, button_whatsApp, button_Oferta1, button_Oferta2, button_Oferta3, percentage, client], (err, results) => {
+          if (err) throw err;
+          console.log('Cliente atualizado com sucesso!');
+        });
 
-        } else {
-            // Se o cliente não existir, insira um novo registro
-            const insertQuery = `
+      } else {
+        // Se o cliente não existir, insira um novo registro
+        const insertQuery = `
                 INSERT INTO faceline (client, datetime, button1, button2, button3, button4, button5, button6, button7, button8, button_whatsApp, button_Oferta1, button_Oferta2, button_Oferta3, percentage)
                 VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
             `;
 
-            connection.query(insertQuery, [client, datetime, button1, button2, button3, button4, button5, button6, button7, button8, button_whatsApp, button_Oferta1, button_Oferta2, button_Oferta3, percentage], (err, results) => {
-                if (err) throw err;
-                console.log('Cliente inserido com sucesso!');
-            });
-        }
+        connection.query(insertQuery, [client, datetime, button1, button2, button3, button4, button5, button6, button7, button8, button_whatsApp, button_Oferta1, button_Oferta2, button_Oferta3, percentage], (err, results) => {
+          if (err) throw err;
+          console.log('Cliente inserido com sucesso!');
+        });
+      }
     });
-}
+  }
 
 
 };
